ec570bc46cca026d7b55fdb5fd1b0c974c798700
RichardKim <hiddeneventor@gmail.com>
2024-01-28 23:43:39 +0900

    [Widget] MaterialApp, Scaffold, StatelessWidget 사용

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,28 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: Text(' Hello! I am a Flutter App.'),
-  ));
+      home: Scaffold(
+    body: TestWidget(),
+  )));
+}
+
+class TestWidget extends StatelessWidget {
+  const TestWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text(
+          'Hello, flutter!',
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
 }
