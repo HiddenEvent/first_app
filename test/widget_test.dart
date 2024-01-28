b44e032bfdecd578b1ecf56b7e7905fe82663ac5
RichardKim <hiddeneventor@gmail.com>
2024-01-28 18:25:14 +0900

    기본 클래스 문법 작성예

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:first_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  Point point = Point(1, 2);
+  print(point);
+  print(point.x);
+  print(point.y);
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+class Point {
+  double? x, y;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  Point(this.x, this.y);
 }
