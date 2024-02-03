e3e59e561cc9235b0d8f517b2c081f726626f0ae
RichardKim <hiddeneventor@gmail.com>
2024-02-03 16:18:34 +0900

    [Container 위젯] 기능 살펴보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-    appBar: AppBar(actions: [
-      IconButton(
-        icon: Icon(Icons.home),
-        onPressed: () {
-          print('Home button is pressed');
-        },
+        appBar: AppBar(
+          title: Text('Study to Container'),
+        ),
+        body: CustomContainer(),
       ),
-      IconButton(
-        icon: Icon(Icons.play_arrow),
-        onPressed: () {
-          print('Play button is pressed');
-        },
-      ),
-    ], centerTitle: false, title: Text('This is App Bar')),
-    body: TestWidget(),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.bug_report),
-      onPressed: () {
-        print('Tab! FAB!');
-      },
     ),
-  )));
+  );
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
-      child: Center(
-        child: Text(
-          'Hello, flutter!',
-          style: TextStyle(
-            fontSize: 40,
-            color: Colors.black,
+    return Center(
+      child: Container(
+        // width: double.infinity, // 좌우 꽉 차게
+        // height: double.infinity, // 상하 꽉 차게
+        width: 300,
+        height: 300,
+        color: Color(0xFF7DCE76),
+        // padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
+        // margin: EdgeInsets.symmetric(vertical: 24, horizontal: 30),
+        child: Center(
+          child: Container(
+            color: Colors.yellow,
+            child: Text('Hello Container'),
           ),
         ),
       ),
