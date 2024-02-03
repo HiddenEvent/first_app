9222f1972f69be93df23b2ff522820cb4c8698ae
RichardKim <hiddeneventor@gmail.com>
2024-02-03 16:27:27 +0900

    [Container 위젯] BoxDecoration - 위젯의 모양과 스타일을 정의하는 데 사용되는 클래스 (위젯 X) * 주의: BoxDecoration 상위에 Color가 정의 되어 있으면 에러 발생

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,17 @@ class CustomContainer extends StatelessWidget {
         // height: double.infinity, // 상하 꽉 차게
         width: 300,
         height: 300,
-        color: Color(0xFF7DCE76),
         // padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
         // margin: EdgeInsets.symmetric(vertical: 24, horizontal: 30),
+        decoration: BoxDecoration(
+          color: Color(0xFF7DCE76),
+          border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(100),
+          boxShadow: [
+            BoxShadow(color: Colors.black.withOpacity(0.3), offset: Offset(10, 10), blurRadius: 10, spreadRadius: 1),
+            BoxShadow(color: Colors.blue.withOpacity(0.3), offset: Offset(10, 10), blurRadius: 10, spreadRadius: 1),
+          ],
+        ),
         child: Center(
           child: Container(
             color: Colors.yellow,
