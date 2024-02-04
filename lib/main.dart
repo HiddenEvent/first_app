e515080eb5b2daeae8d8149229b55e316b183fc2
RichardKim <hiddeneventor@gmail.com>
2024-02-04 16:38:43 +0900

    [비율 처리] Expanded, Flexible - 화면의 비율을 사용하는 기능, 둘다 화면 비율 처리하는 위젯임 - Flexible: 내부 size가 우선 적용된다 - Expanded: 내부 size를 무시하고 비율로 설정됨

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Widget을 상하좌우 모두 배치하기'),
+          title: Text('Widget을 비율로 배치하기'),
         ),
         body: Body(),
       ),
@@ -20,80 +20,17 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center, // 세로 정렬
-        crossAxisAlignment: CrossAxisAlignment.start, // 가로 정렬|
-        children: [
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-          Container(
-            color: Colors.grey,
-            width: 100,
-            height: 100,
-            margin: EdgeInsets.symmetric(horizontal: 8),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        Container(width: double.infinity, height: 200, color: Colors.red),
+        Expanded(child: Container(color: Colors.blue, height: 100)),
+        Flexible(child: Container(color: Colors.red, height: 100)),
+
+        // Flexible(flex: 1, child: Container(color: Colors.red)),
+        // Flexible(flex: 2, child: Container(color: Colors.blue)),
+        // Flexible(flex: 3, child: Container(color: Colors.green)),
+        // Flexible(flex: 4, child: Container(color: Colors.yellow)),
+      ],
     );
   }
 }
