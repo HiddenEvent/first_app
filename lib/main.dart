9aa12108d80ac32bf3108d7bde7f11e0a63a5d43
RichardKim <hiddeneventor@gmail.com>
2024-02-04 23:03:47 +0900

    [Stateless vs Stateful]

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Widget을 비율로 배치하기'),
-        ),
+        appBar: AppBar(title: Text('Stateless vs Stateful')),
         body: Body(),
       ),
     ),
@@ -20,20 +18,72 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
-        Container(width: 500, height: 500, color: Colors.black),
-        Container(width: 400, height: 400, color: Colors.red),
-        Container(width: 300, height: 300, color: Colors.green),
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
-          ),
-        )
+        ExampleStateless(),
+        ExampleStateful(index: 3),
       ],
+    );
+  }
+}
+
+class ExampleStateless extends StatelessWidget {
+  const ExampleStateless({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  final int index;
+
+  const ExampleStateful({super.key, required this.index});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  late int _index;
+  late TextEditingController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _index = widget.index;
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (_index == 5) {
+              _index = 0;
+            }
+            _index++;
+          });
+        },
+        child: Container(
+          color: Colors.blue.withOpacity(_index / 5),
+          child: Center(
+            child: Text('$_index'),
+          ),
+        ),
+      ),
     );
   }
 }
