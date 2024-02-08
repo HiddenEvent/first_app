4533ec719092f8cbf97ba48b1d60e9966005bfab
RichardKim <hiddeneventor@gmail.com>
2024-02-08 21:29:37 +0900

    [로컬 자원 활용하기]

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+const assetImagePath = 'assets/images/';
+const bannerImage = '${assetImagePath}illustrator-basic-20-overview.webp';
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('외부라이브러리 사용하기')),
+        appBar: AppBar(title: const Text('로컬 자원 활용하기')),
         body: const Body(),
       ),
     ),
@@ -20,29 +23,8 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return RandomWords();
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({super.key});
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordList = generateWordPairs().take(5).toList();
-    final widgets = wordList
-        .map(
-          (word) => Text(word.asCamelCase, style: const TextStyle(fontSize: 30)),
-        )
-        .toList();
-
-    return Column(
-      children: widgets,
+    return Center(
+      child: Image.asset(bannerImage),
     );
   }
 }
