370b1ac65b98f85ff3d90ee33cfcd554f5bef55d
RichardKim <hiddeneventor@gmail.com>
2024-02-10 23:43:11 +0900

    [인스타 클론] init

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
-import 'package:first_app/game/game_body.dart';
 import 'package:flutter/material.dart';
 
-const assetImagePath = 'assets/images/';
-const bannerImage = '${assetImagePath}illustrator-basic-20-overview.webp';
-
 void main() {
-  runApp(const RSPApp());
+  runApp(const InstaCloneApp());
 }
 
-class RSPApp extends StatelessWidget {
-  const RSPApp({super.key});
+class InstaCloneApp extends StatelessWidget {
+  const InstaCloneApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('가위 바위 보!'),
-        ),
-        body: const GameBody(),
-      ),
+      home: InstaCloneHome(),
     );
+  }
+}
+
+class InstaCloneHome extends StatelessWidget {
+  const InstaCloneHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
--- lib/style/theme.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-final customTheme = ThemeData(
-  colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-  textTheme: const TextTheme(
-    bodyMedium: TextStyle(fontWeight: FontWeight.normal, fontSize: 30, color: Colors.black),
-  ),
-  useMaterial3: false,
-);
