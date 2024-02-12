65fb02888c24cc4308e8bb1be2bc252b8a217099
RichardKim <hiddeneventor@gmail.com>
2024-02-12 22:24:20 +0900

    [인스타 클론] UserStory 처리 (좌우 스크롤)

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -21,7 +21,41 @@ class StoryArea extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(10, (index) => UserStory(userName: 'User $index')),
+      ),
+    );
+  }
+}
+
+class UserStory extends StatelessWidget {
+  final String userName;
+  const UserStory({
+    super.key,
+    required this.userName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+      child: Column(
+        children: [
+          Container(
+            width: 80,
+            height: 80,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            decoration: BoxDecoration(
+              color: Colors.blue.shade300,
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+          Text(userName)
+        ],
+      ),
+    );
   }
 }
 
